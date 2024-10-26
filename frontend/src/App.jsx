f0505bc146c20daea296e4fcd3b05d44363e1c75
livinusonynso <livinusonyenso@gmail.com>
2024-10-26 16:02:45 +0100

    first commit of header and product details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './App.css'
 import Homepage from './pages/Homepage'
+import ProductDetails from './components/product-Details/ProductDetails'
+import Header from './components/header/Header'
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
   return (
     <div>
-      <Homepage />
+      {/* <Homepage /> */}
+      <Header />
+      <ProductDetails/>
+      
     </div>
     
   )
