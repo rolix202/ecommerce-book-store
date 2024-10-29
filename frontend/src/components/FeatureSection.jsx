ef790f41dc35aea20e5e84237c595c937fe88ab1
rolix202 <williamsroland147@gmail.com>
2024-10-29 17:51:01 +0100

    added the free shipping section

--- frontend/src/components/FeatureSection.jsx
+++ frontend/src/components/FeatureSection.jsx
@@ -1,3 +1,4 @@
+import { BookOpenIcon, StarIcon, TruckIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
 const FeatureSection = () => {
@@ -5,12 +6,15 @@ const FeatureSection = () => {
     <div className='wrapper'>
         <div className="feature-container">
             <div className="ship">
-                <span>Free shiping over 50$ </span>
+                <TruckIcon className='icon'/>
+                <span> Free shiping over 50$ </span>
             </div>
             <div className="royalty">
+                <StarIcon className='icon'/>
                 <span>Save with loyalty points</span>
             </div>
             <div className="read-pages">
+                <BookOpenIcon className='icon'/>
                 <span>Read a few pages </span>
             </div>
         </div>
---
+++ frontend/src/components/SelectedForYou.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SelectedForYou = () => {
+  return (
+    <div className='wrapper'>
+        <div className="selected4u">
+            <h2>Selected for you</h2>
+        </div>
+        
+    </div>
+  )
+}
+
+export default SelectedForYou
