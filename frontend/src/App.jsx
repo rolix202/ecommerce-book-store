6c097b450b3d16651cd6db6531e78dfb77dc6000
livinusonynso <livinusonyenso@gmail.com>
2024-10-30 07:53:20 +0100

    fixed currency sign and footer section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fontsource/inter";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Homepage from './pages/Homepage'
